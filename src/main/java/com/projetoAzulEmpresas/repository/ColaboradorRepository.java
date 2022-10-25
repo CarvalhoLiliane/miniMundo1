847fdaf685ca7f1c8e0607c2746760aa0db2e436
Liliane Antunes <lilianeantunes33@gmail.com>
2022-10-25 20:40:09 -0300

    Incluida anotation na classe repository

--- src/main/java/com/projetoAzulEmpresas/repository/ColaboradorRepository.java
+++ src/main/java/com/projetoAzulEmpresas/repository/ColaboradorRepository.java
@@ -1,9 +1,11 @@
 package com.projetoAzulEmpresas.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.projetoAzulEmpresas.model.Colaborador;
 
+@Repository
 public interface ColaboradorRepository extends JpaRepository<Colaborador, Long> {
 
 }
--- src/main/java/com/projetoAzulEmpresas/repository/SetorRepository.java
+++ src/main/java/com/projetoAzulEmpresas/repository/SetorRepository.java
@@ -1,9 +1,11 @@
 package com.projetoAzulEmpresas.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.projetoAzulEmpresas.model.Setor;
 
+@Repository
 public interface SetorRepository extends JpaRepository<Setor, Long> {
 
 }
